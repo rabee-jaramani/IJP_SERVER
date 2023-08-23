0fd5d261bd393c3dcda437bab82cd017ffc16f50
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2023-08-23 22:11:06 +0400

    Update server.js

--- server.js
+++ server.js
@@ -11,46 +11,46 @@ app.use(cors());
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// // Set up nodemailer transporter
-// const transporter = nodemailer.createTransport({
-//     service: 'SMTP',
-//     host: 'eu-smtp-outbound-1.mimecast.com',
-//     port: 587,
-//     secure: true, // use SSL
-//     auth: {
-//         user: 'ijp@apparelglobal.com', // Your email
-//         pass: '64Ld!*52zXAtpVYt&XT$XOEsa', // Your email password
-//     },
-// });
+// Set up nodemailer transporter
+const transporter = nodemailer.createTransport({
+    service: 'SMTP',
+    host: 'eu-smtp-outbound-1.mimecast.com',
+    port: 587,
+    secure: true, // use SSL
+    auth: {
+        user: 'ijp@apparelglobal.com', // Your email
+        pass: '64Ld!*52zXAtpVYt&XT$XOEsa', // Your email password
+    },
+});
 
-// // API endpoint to send email with attachment
-// app.post('/send-email', upload.single('attachment'), (req, res) => {
-//     try {
-//         const { to, subject, text } = req.body;
+// API endpoint to send email with attachment
+app.post('/send-email', upload.single('attachment'), (req, res) => {
+    try {
+        const { to, subject, text } = req.body;
 
-//         var mailOptions = {
-//             from: 'ijp@apparelglobal.com', // Your email
-//             to: to,
-//             subject: subject,
-//             text: text,
-//         };
-//     } catch (error) {
-//         res.send("Error from post request", error)
-//     }
-//     transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//             console.error(error);
-//             res.send('error X', error)
-//         } else {
-//             console.log('Email sent:', info.response);
-//             res.send('Email sent successfully');
-//         }
-//     });
-// });
-// transporter.verify((err, success) => {
-//     if (err) console.error(err);
-//     if (success) console.log('Your config is correct');
-// });
+        var mailOptions = {
+            from: 'ijp@apparelglobal.com', // Your email
+            to: to,
+            subject: subject,
+            text: text,
+        };
+    } catch (error) {
+        res.send("Error from post request", error)
+    }
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.send('error X', error)
+        } else {
+            console.log('Email sent:', info.response);
+            res.send('Email sent successfully');
+        }
+    });
+});
+transporter.verify((err, success) => {
+    if (err) console.error(err);
+    if (success) console.log('Your config is correct');
+});
 
 
 
